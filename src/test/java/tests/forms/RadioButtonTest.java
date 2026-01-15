dc6284f37242f90827767fa86e933c233f735bc2
Damilyaa <damilyaamangeldykyzy@gmail.com>
2026-01-15 07:43:56 +0500

    semi final

---
+++ src/test/java/tests/forms/RadioButtonTest.java
@@ -0,0 +1,4 @@
+package tests.forms;
+
+public class RadioButtonTest {
+}
---
+++ src/test/java/tests/javascript/JavaScriptTest.java
@@ -0,0 +1,12 @@
+package javascript;
+
+import org.testng.annotations.Test;
+import base.BaseTest;
+
+public class JavaScriptTest extends BaseTest {
+
+    @Test
+    public void testScrollingToElement(){
+
+    }
+}
